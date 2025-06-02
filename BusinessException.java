e151323e264537e43d70048323837a27465078d5
madruga0 <pedromadruga6@gmail.com>
2025-06-02 14:07:18 -0300

    Some files were removed

--- BusinessException.java
+++
@@ -1,8 +0,0 @@
-public class BusinessException extends RuntimeException {
-    
-    private static final long serialVersionUUID = 1L;
-
-    public BusinessException(String msg) {
-        super(msg);
-    }
-}
