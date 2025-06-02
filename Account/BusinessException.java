d96eab3d6f3c3cdaf799c55a6d5f9589b703c8f7
madruga0 <pedromadruga6@gmail.com>
2025-06-02 13:59:30 -0300

    Some changes were maked

---
+++ Account/BusinessException.java
@@ -0,0 +1,8 @@
+public class BusinessException extends RuntimeException {
+    
+    private static final long serialVersionUUID = 1L;
+
+    public BusinessException(String msg) {
+        super(msg);
+    }
+}
